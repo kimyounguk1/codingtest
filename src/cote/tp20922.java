1f990ebfd4c3821950b609c1645f6411fffe5c48
kimyounguk1 <kyw020108@gmail.com>
2025-08-11 01:12:27 +0900

    no message

--- src/cote/tp20922.java
+++ src/cote/tp20922.java
@@ -15,7 +15,7 @@ public class tp20922 {
 		int k = Integer.parseInt(st1.nextToken());
 		
 		int[] arr = new int[n];
-		int[] state = new int[2000001];
+		int[] state = new int[1000001];
 		
 		StringTokenizer st2 = new StringTokenizer(br.readLine());
 		
@@ -23,19 +23,21 @@ public class tp20922 {
 			arr[i] = Integer.parseInt(st2.nextToken());
 		}
 		
-		int count = 0;
-		int max = 0;
+		int len = 0;
+		int start = 0;
+		int end = 0;
 		
-		for(int i=0; i<n; i++) {
-			state[arr[i]]++;
-			if(state[arr[i]]<=k) count++;
-			if(state[arr[i]]>k) {
-				max = Math.max(max, count);
-				count = 0;
+		while(end<arr.length) {
+			state[arr[end]]++;
+			while(state[arr[end]]>k) {
+				state[arr[start]]--;
+				start++;
 			}
+			len = Math.max(len, end-start+1);
+			end++;
 		}
 		
-		System.out.println(count);
+		System.out.println(len);
 	}
 
 }
